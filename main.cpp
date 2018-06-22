1124122bfa9233131d77fded87f3fb6e47958b2a
Nguyen Hoang Duong <you_create@protonmail.com>
2018-06-22 21:32:04 +0700

    Change some strings.

--- main.cpp
+++ main.cpp
@@ -6,23 +6,25 @@ int main(int argc, char const *argv[])
 {
     if ((argc == 1) || (argc == 2 && (argv[1] == std::string("-h") || argv[1] == std::string("--help"))))
     {
-        std::cout << argv[0] << " [regex] (directory)" << std::endl
-                  << std::endl
-                  << "regex     : regex used to find in files" << std::endl
-                  << "directory : (Optional) look up location" << std::endl;
+        std::cout << "wfind - Find files whose contents match the given search regular expression"
+                  << std::endl << std::endl
+                  << argv[0] << " [regex] (directory)" << std::endl << std::endl
+                  << "  regex     : regex used to find in files" << std::endl
+                  << "  directory : (Optional) look up location" << std::endl;
         return 0;
     }
+
     std::string keyword = std::string(argv[1]);
 
     wfind sif;
     if (argc > 2)
         if (!sif.changeDirectory(fs::path(std::string(argv[2]))))
         {
-            std::cout << "invalid directory" << std::endl;
+            std::cout << "Directory does not exist." << std::endl;
             return 1;
         }
 
-    std::cout << "Cwd: " << sif.currentDirectory() << std::endl;
+    std::cout << "Looking up directory: " << sif.currentDirectory() << std::endl;
     std::cout << "Regex: \"" << keyword << "\"" << std::endl;
 
     std::vector<fs::directory_entry> result;
@@ -32,14 +34,20 @@ int main(int argc, char const *argv[])
     }
     catch (std::regex_error e)
     {
-        std::cout << "regex error" << std::endl;
+        std::cout << "Regex error." << std::endl;
     }
+
     for (auto &&iter : result)
         std::cout << iter << std::endl;
     if (result.empty())
-        std::cout << "no files found" << std::endl;
+    {
+        std::cout << "No matches found." << std::endl;
+    }
     else
-        std::cout << result.size() << " file(s) found" << std::endl;
+    {
+        std::cout << result.size() << " file(s) found." << std::endl;
+    }
+
 
     return 0;
 }
