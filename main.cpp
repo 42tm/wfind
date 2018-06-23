73777842181647f38bab32452c01a878b3ed2405
Dung Winux <ntddebugger@gmail.com>
2018-06-23 14:21:40 +0700

    Add if case

--- main.cpp
+++ main.cpp
@@ -4,8 +4,31 @@
 int main(int argc, char const *argv[])
 {
     std::cout
-        << "wfind - Find files whose contents match the given search regular expression" << std::endl
-        << "  built with gcc " << __GNUC__ << "." << __GNUC_MINOR__ << "." << __GNUC_PATCHLEVEL__ << "\t" << std::endl;
+        << "wfind - Find files whose contents match the given search regular expression" << std::endl;
+
+// Check compiler's name & version
+#if defined(__GNUC__)
+    std::cout
+        << "  built with gcc " << __GNUC__ << "." << __GNUC_MINOR__ << "." << __GNUC_PATCHLEVEL__;
+        
+#if defined(__MINGW64__)
+    std::cout << " (MinGW-w64 64bit " << __MINGW64_VERSION_MAJOR << "." << __MINGW64_VERSION_MINOR << ")";
+#elif defined(__MINGW32__)
+    std::cout << " (MinGW/MinGW-w64 32bit " << __MINGW32_VERSION_MAJOR << "." << __MINGW32_VERSION_MINOR << ")";
+#endif
+
+#ifdef __CYGWIN__
+    std::cout << " (Cygwin)";
+#endif
+
+    std::cout << std::endl;
+#elif defined(_MSC_VER)
+    std::cout
+        << "  built with MS Visual C++ " << _MSC_FULL_VER << std::endl;
+#else
+    std::cout << "  built with unknown C++ compiler" << std::endl;
+#endif
+
     if ((argc == 1) || (argc == 2 && (argv[1] == std::string("-h") || argv[1] == std::string("--help"))))
     {
         std::cout
