55d04f40093ee181c1e950fb8f256d53ef6447ae
Nguyen Hoang Duong <you_create@protonmail.com>
2018-06-23 15:35:11 +0700

    Revive wfine.hpp

---
+++ lib/wfind.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <regex>
+
+#if (__cplusplus < 201703L || __GNUC__ < 8)
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+#else
+#include <filesystem>
+namespace fs = std::filesystem;
+#endif
+
+class wfind
+{
+  private:
+    fs::path curWorkingDir;
+    bool contentMatch(fs::path filename, std::string keyword);
+
+  public:
+    wfind(fs::path path = fs::current_path()) { changeDirectory(path); }
+    std::vector<fs::directory_entry> searchDirectory(std::string keyword);
+    bool changeDirectory(fs::path path)
+    {
+        if (!fs::exists(path) || !fs::is_directory(fs::status(path)))
+            return false;
+        this->curWorkingDir = path;
+        return true;
+    }
+    const fs::path &currentDirectory() { return curWorkingDir; }
+};
